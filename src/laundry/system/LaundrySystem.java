cecf52daf8d3ebb75d8311f6f26ae2ba23801a0f
Lyka Joyce Moros <90735386+lawleit06@users.noreply.github.com>
2024-12-24 13:29:13 +0800

    Add files via upload

--- src/laundry/system/LaundrySystem.java
+++ src/laundry/system/LaundrySystem.java
@@ -23,10 +23,11 @@ import java.time.format.DateTimeFormatter;
  */
 public class LaundrySystem {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/laundry_manager?zeroDateTimeBehavior=CONVERT_TO_NULL";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/laundry_manager?zeroDateTimeBehavior=CONVERT_TO_NULL";
     private static Connection conn;
     
     public static void main(String[] args) {
+        
         if (connect()) {
             System.out.println("Database connected successfully!");
             fetchData(conn);
@@ -34,7 +35,10 @@ public class LaundrySystem {
             System.err.println("Database connection failed: ");
         }
         new Laundry_Interface().setVisible(true);
+        
     }
+    
+     
     // testing method
     private static void fetchData(Connection connection) {
         String query = "SELECT * FROM accounts";
@@ -127,18 +131,16 @@ public class LaundrySystem {
         String query = "SELECT laundry_log.laundry_id, laundry_log.laundry_weight, laundry_log.laundry_received_time, " +
                "laundry_log.laundry_claimed_time, customer_log.customer_id, customer_log.first_name, " +
                "customer_log.last_name, customer_log.contact_number, laundry_status.status_name AS laundry_status, " +
-               "services.service_name AS laundry_service, services.price_per_kg, " +
-               "payment_status.payment_status_name " +
-               "FROM laundry_log " +
+               "services.service_name AS laundry_service, " +
+               "services.price_per_kg FROM laundry_log " +
                "JOIN customer_log ON laundry_log.laundry_owner = customer_log.customer_id " +
                "JOIN services ON laundry_log.laundry_service = services.service_id " +
-               "JOIN payment_status ON laundry_log.payment_status = payment_status.payment_id " +
                "JOIN laundry_status ON laundry_log.laundry_status = laundry_status.status_id " +
                "WHERE laundry_log.laundry_status != 5";
         
         DefaultTableModel tableModel = new DefaultTableModel(new String[]{
                 "Laundry ID",  "Name", "Contact Number", "Weight (kg)",
-                "Service Type", "Price" , "Payment Status", "Received Time", "Laundry Status"
+                "Service Type", "Price" , "Received Time", "Laundry Status"
             }, 0);
         
         try (PreparedStatement pst = conn.prepareStatement(query);
@@ -154,7 +156,6 @@ public class LaundrySystem {
                 row.add(weight);
                 row.add(resultSet.getString("laundry_service"));
                 row.add(price_kg * weight);
-                row.add(resultSet.getString("payment_status_name"));
                 row.add(resultSet.getString("laundry_received_time"));
                 row.add(resultSet.getString("laundry_status"));
                 tableModel.addRow(row);
@@ -166,15 +167,14 @@ public class LaundrySystem {
         return tableModel;
     }
     // add to queue
-    public static boolean addQueue(int owner, float weight, int service, int payment_status, String date, int status){
-        String query = "INSERT INTO laundry_log (laundry_weight, laundry_owner, laundry_service, payment_status, laundry_received_time, laundry_status) VALUES (?, ?, ?, ?, ?, ?)";
+    public static boolean addQueue(int owner, float weight, int service, String date, int status){
+        String query = "INSERT INTO laundry_log (laundry_weight, laundry_owner, laundry_service, laundry_received_time, laundry_status) VALUES (?, ?, ?, ?, ?)";
         try (PreparedStatement preparedStatement = conn.prepareStatement(query)) {
             preparedStatement.setFloat(1, weight);
             preparedStatement.setInt(2, owner);
             preparedStatement.setInt(3, service);
-            preparedStatement.setInt(4, payment_status);
-            preparedStatement.setString(5, date);
-            preparedStatement.setInt(6, status);
+            preparedStatement.setString(4, date);
+            preparedStatement.setInt(5, status);
             int rowsAffected = preparedStatement.executeUpdate();
             return rowsAffected > 0; // Returns true if the insertion was successful
         } catch (SQLException ex) {
@@ -182,22 +182,7 @@ public class LaundrySystem {
             return false;
         }
     }
-    // edit queue
-    public static boolean editQueue(int laundry_id, int payment_status, int laundry_status, String dNt){
-        String query = "UPDATE laundry_log SET laundry_claimed_time = ?, laundry_status = ?, payment_status = ? WHERE laundry_id = ?";
-        try (PreparedStatement pst = conn.prepareStatement(query)){
-            pst.setString(1, dNt);
-            pst.setInt(2, laundry_status);
-            pst.setInt(3, payment_status);
-            pst.setInt(4, laundry_id);
-            return pst.executeUpdate() > 0;
-        } catch (SQLException ex) {
-            Logger.getLogger(LaundrySystem.class.getName()).log(Level.SEVERE, "Error editing customer", ex);
-            return false;
-        }
-    }
-
-    // get service fee
+    // get current date and time
     public static float fetchServiceFee(int service){
         String query = "SELECT * FROM services " + "WHERE services.service_id = ?";
         float price = 0;
@@ -210,7 +195,7 @@ public class LaundrySystem {
             System.err.println("Error executing query: " + e.getMessage()); }
         return price;
     }
-    // get current date and time
+    
     public static String getDateNTime(){
         LocalDateTime now = LocalDateTime.now();
 
